f68b62e77f77e32595c5ec6c580b5915865790db
khang <khanghugo123@gmail.com>
2020-07-09 18:41:18 -0400

    nothing, void, dont attempt

--- nyaasi_hoarder_gui.py
+++ nyaasi_hoarder_gui.py
@@ -1,1 +1,31 @@
 from nyaasi_hoarder import nyaasi_hoarder
+import tkinter as tk
+
+
+class nyaasi_hoarder_gui:
+	def __init__(self, master):
+
+		self.master = master
+
+		self.createMainMenu(self.master)
+
+
+
+	def createMainMenu(self, master):
+		
+		master.title("Nyaasi Hoarder")
+		master.geometry("500x500")
+		master.resizable(0,0)
+		master.mainloop()
+
+
+def main():
+	mainMenu = tk.Tk()
+
+	nyaasi_hoarder_gui(mainMenu)
+
+
+
+
+if __name__ == "__main__":
+	main()
